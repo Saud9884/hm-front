a37c6ede48ef211ce701ecf3f11d34c934775d92
Saud9884 <saudlohar85@gmail.com>
2023-12-04 17:17:14 +0530

    Added Header section to HomePage

---
+++ components/Header.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import styled from "styled-components";
+import Center from "./Center";
+
+
+export default function Header(){
+    const StyleHeader = styled.header`
+    background-color: #222;
+    `;
+    const Logo = styled(Link)`
+    color:#fff;
+    text-decoration:none;
+    `;
+    const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 0;
+    `;
+    const StyledNav = styled.nav`
+    display: flex;
+    gap: 15px;
+    `;
+    const NavLink = styled(Link)`
+    color:#aaa;
+    text-decoration: none;
+    `;
+    return (
+        <StyleHeader>
+            <Center>
+                <Wrapper>
+            <Logo href={'/'}>HM Tractor Parts</Logo>
+            <StyledNav>
+                <NavLink href={'/'}>Home</NavLink>
+                <NavLink href={'/products'}>All Products</NavLink>
+                <NavLink href={'/categories'}>Categories</NavLink>
+                <NavLink href={'/account'}>Account</NavLink>
+                <NavLink href={'/cart'}>Cart (0)</NavLink>
+            </StyledNav>
+            </Wrapper>
+            </Center>
+        </StyleHeader>
+    );
+}
