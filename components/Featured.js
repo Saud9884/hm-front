10e43009e0c6f23eacfbc33ff89a7dbf93537431
Saud9884 <saudlohar85@gmail.com>
2023-12-05 15:27:18 +0530

    Added banner to the home page

--- components/Featured.js
+++ components/Featured.js
@@ -1,55 +1,25 @@
 import styled from "styled-components";
-import Center from "./Center";
+import Image from "next/image";
+import Link from "next/link";
 
-const Bg = styled.div`
-  background-color: #222;
-  color: #fff;
-  padding: 50px 0;
-`;
 
-const Title = styled.h1`
-  margin: 0;
-  font-weight: normal;
-`;
-
-const Desc = styled.p`
-  color: #aaa;
-  font-size: 0.8rem;
-`;
-
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: 0.8fr 1.2fr;
-  gap: 40px;
-  img {
-    max-width: 100%;
-    
-  }
-  div{
-    align-item
-  }
+const StyledImage = styled(Image)`
+  object-fit: cover; /* This will make the image cover the container while maintaining its aspect ratio */
+  width: 100%;
+  height: 100%;
 `;
 export default function Featured() {
   return (
     <div>
-      <Bg>
-        <Center>
-          <Wrapper>
-            <div>
-              <Title>Featured 1</Title>
-              <Desc>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae recusandae soluta non ducimus iste quod laborum minus facere autem, quibusdam assumenda, distinctio voluptatem itaque sit maxime eveniet possimus repellat praesentium?
-              </Desc>
-            </div>
-            <div>
-              <img
-                src="https://4.imimg.com/data4/KJ/BY/MY-14831048/john-deere-5050d-tractor.jpg"
-                alt=""
-              />
-            </div>
-          </Wrapper>
-        </Center>
-      </Bg>
+      
+      <Link href="/products">
+        
+
+          <StyledImage src="/banner.jpg" alt="banner" width={1270} height={321}  layout="responsive"/>
+          
+          </Link>
+        
+      
     </div>
   );
 }
