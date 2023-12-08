0a65fc0c6591528eae0b39d4461e9d17dc848e74
Saud9884 <saudlohar85@gmail.com>
2023-12-08 18:09:32 +0530

    add coupon secition

--- pages/cart.js
+++ pages/cart.js
@@ -2,8 +2,8 @@ import Header from "@/components/Header";
 import styled from "styled-components";
 import Center from "@/components/Center";
 import Button from "@/components/Button";
-import {useContext, useEffect, useState} from "react";
-import {CartContext} from "@/components/CartContext";
+import { useContext, useEffect, useState } from "react";
+import { CartContext } from "@/components/CartContext";
 import axios from "axios";
 import Table from "@/components/Table";
 import Input from "@/components/Input";
@@ -13,7 +13,7 @@ const ColumnsWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   @media screen and (min-width: 768px) {
-    grid-template-columns: 1.2fr .8fr;
+    grid-template-columns: 1.2fr 0.8fr;
   }
   gap: 40px;
   margin-top: 40px;
@@ -34,11 +34,11 @@ const ProductImageBox = styled.div`
   height: 100px;
   padding: 2px;
   border: 1px solid rgba(0, 0, 0, 0.1);
-  display:flex;
+  display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 10px;
-  img{
+  img {
     max-width: 60px;
     max-height: 60px;
   }
@@ -46,7 +46,7 @@ const ProductImageBox = styled.div`
     padding: 10px;
     width: 100px;
     height: 100px;
-    img{
+    img {
       max-width: 80px;
       max-height: 80px;
     }
@@ -63,36 +63,37 @@ const QuantityLabel = styled.span`
 `;
 
 const CityHolder = styled.div`
-  display:flex;
+  display: flex;
   gap: 5px;
 `;
 
 export default function CartPage() {
-   const {cartProducts,addProduct,removeProduct,clearCart} = useContext(CartContext);
-  const [products,setProducts] = useState([]);
-  const [name,setName] = useState('');
-  const [email,setEmail] = useState('');
-  const [city,setCity] = useState('');
-   const [postalCode,setPostalCode] = useState('');
-   const [streetAddress,setStreetAddress] = useState('');
-   const [country,setCountry] = useState('');
-   const [phoneNumber,setPhoneNumber] = useState('');
-  const [isSuccess,setIsSuccess] = useState(false);
+  const { cartProducts, addProduct, removeProduct, clearCart } =
+    useContext(CartContext);
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [country, setCountry] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [couponCode, setCouponCode] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
   useEffect(() => {
     if (cartProducts.length > 0) {
-      axios.post('/api/cart', {ids:cartProducts})
-        .then(response => {
-          setProducts(response.data);
-        })
+      axios.post("/api/cart", { ids: cartProducts }).then((response) => {
+        setProducts(response.data);
+      });
     } else {
       setProducts([]);
     }
   }, [cartProducts]);
   useEffect(() => {
-    if (typeof window === 'undefined') {
+    if (typeof window === "undefined") {
       return;
     }
-    if (window?.location.href.includes('success')) {
+    if (window?.location.href.includes("success")) {
       setIsSuccess(true);
       clearCart();
     }
@@ -104,8 +105,13 @@ export default function CartPage() {
     removeProduct(id);
   }
   async function goToPayment() {
-    const response = await axios.post('/api/checkout', {
-      name,email,city,postalCode,streetAddress,country,
+    const response = await axios.post("/api/checkout", {
+      name,
+      email,
+      city,
+      postalCode,
+      streetAddress,
+      country,
       cartProducts,
     });
     if (response.data.url) {
@@ -114,8 +120,30 @@ export default function CartPage() {
   }
   let total = 0;
   for (const productId of cartProducts) {
-    const price = products.find(p => p._id === productId)?.price || 0;
+    const price = products.find((p) => p._id === productId)?.price || 0;
     total += price;
+  }
+
+  function applyCouponCode() {
+    // Replace this with your actual coupon validation logic
+    const validCouponCode = "DISCOUNT10";
+
+    if (couponCode === validCouponCode) {
+      // Your logic to calculate the discount percentage
+      const discountPercentage = 10; // 10% discount for the example
+
+      // Calculate the discount amount
+      const discountAmount = (total * discountPercentage) / 100;
+
+      // Apply the discount to the total
+      total -= discountAmount;
+
+      // You can also add additional logic here, such as updating the UI or showing a message
+      console.log(`Coupon applied successfully! Discount: $${discountAmount.toFixed(2)}`);
+    } else {
+      // Invalid coupon code, you can add logic to handle this case (e.g., show an error message)
+      console.error("Invalid coupon code");
+    }
   }
 
   if (isSuccess) {
@@ -140,9 +168,7 @@ export default function CartPage() {
         <ColumnsWrapper>
           <Box>
             <h2>Cart</h2>
-            {!cartProducts?.length && (
-              <div>Your cart is empty</div>
-            )}
+            {!cartProducts?.length && <div>Your cart is empty</div>}
             {products?.length > 0 && (
               <Table>
                 <thead>
@@ -153,32 +179,39 @@ export default function CartPage() {
                   </tr>
                 </thead>
                 <tbody>
-                  {products.map(product => (
+                  {products.map((product) => (
                     <tr key={product._id}>
                       <ProductInfoCell>
                         <ProductImageBox>
-                        <CldImage
-            width="960"
-            height="800"
-            src={product.images?.[0]}
-            sizes="100vw"
-            alt="Product Image"
-          />
+                          <CldImage
+                            width="960"
+                            height="800"
+                            src={product.images?.[0]}
+                            sizes="100vw"
+                            alt="Product Image"
+                          />
                         </ProductImageBox>
                         {product.title}
                         {product.serial}
                       </ProductInfoCell>
                       <td>
-                        <Button
-                          onClick={() => lessOfThisProduct(product._id)}>-</Button>
+                        <Button onClick={() => lessOfThisProduct(product._id)}>
+                          -
+                        </Button>
                         <QuantityLabel>
-                          {cartProducts.filter(id => id === product._id).length}
+                          {
+                            cartProducts.filter((id) => id === product._id)
+                              .length
+                          }
                         </QuantityLabel>
-                        <Button
-                          onClick={() => moreOfThisProduct(product._id)}>+</Button>
+                        <Button onClick={() => moreOfThisProduct(product._id)}>
+                          +
+                        </Button>
                       </td>
                       <td>
-                        ${cartProducts.filter(id => id === product._id).length * product.price}
+                        $
+                        {cartProducts.filter((id) => id === product._id)
+                          .length * product.price}
                       </td>
                     </tr>
                   ))}
@@ -190,49 +223,71 @@ export default function CartPage() {
                 </tbody>
               </Table>
             )}
+             <Input
+            type="text"
+            placeholder="Coupon Code"
+            value={couponCode}
+            onChange={(ev) => setCouponCode(ev.target.value)}
+          />
+          <Button black block onClick={applyCouponCode}>
+            Apply Coupon
+          </Button>
           </Box>
           {!!cartProducts?.length && (
             <Box>
               <h2>Order information</h2>
-              <Input type="text"
-                     placeholder="Name"
-                     value={name}
-                     name="name"
-                     onChange={ev => setName(ev.target.value)} />
-              <Input type="text"
-                     placeholder="Email"
-                     value={email}
-                     name="email"
-                     onChange={ev => setEmail(ev.target.value)}/>
+              <Input
+                type="text"
+                placeholder="Name"
+                value={name}
+                name="name"
+                onChange={(ev) => setName(ev.target.value)}
+              />
+              <Input
+                type="text"
+                placeholder="Email"
+                value={email}
+                name="email"
+                onChange={(ev) => setEmail(ev.target.value)}
+              />
               <CityHolder>
-                <Input type="text"
-                       placeholder="City"
-                       value={city}
-                       name="city"
-                       onChange={ev => setCity(ev.target.value)}/>
-                <Input type="text"
-                       placeholder="Postal Code"
-                       value={postalCode}
-                       name="postalCode"
-                       onChange={ev => setPostalCode(ev.target.value)}/>
+                <Input
+                  type="text"
+                  placeholder="City"
+                  value={city}
+                  name="city"
+                  onChange={(ev) => setCity(ev.target.value)}
+                />
+                <Input
+                  type="text"
+                  placeholder="Postal Code"
+                  value={postalCode}
+                  name="postalCode"
+                  onChange={(ev) => setPostalCode(ev.target.value)}
+                />
               </CityHolder>
-              <Input type="text"
-                     placeholder="Street Address"
-                     value={streetAddress}
-                     name="streetAddress"
-                     onChange={ev => setStreetAddress(ev.target.value)}/>
-              <Input type="text"
-                     placeholder="Country"
-                     value={country}
-                     name="country"
-                     onChange={ev => setCountry(ev.target.value)}/>
-                     <Input type="text"
-                     placeholder="Phone Number"
-                     value={phoneNumber}
-                     name="pnumber"
-                     onChange={ev => setPhoneNumber(ev.target.value)}/>
-              <Button black block
-                      >
+              <Input
+                type="text"
+                placeholder="Street Address"
+                value={streetAddress}
+                name="streetAddress"
+                onChange={(ev) => setStreetAddress(ev.target.value)}
+              />
+              <Input
+                type="text"
+                placeholder="Country"
+                value={country}
+                name="country"
+                onChange={(ev) => setCountry(ev.target.value)}
+              />
+              <Input
+                type="text"
+                placeholder="Phone Number"
+                value={phoneNumber}
+                name="pnumber"
+                onChange={(ev) => setPhoneNumber(ev.target.value)}
+              />
+              <Button black block>
                 Continue to payment
               </Button>
             </Box>
