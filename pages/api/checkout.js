29713fc61714067d79c123095706c34a2766c0eb
Saud9884 <saudlohar85@gmail.com>
2023-12-15 17:36:40 +0530

    Fixed Stripe webhook

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -45,7 +45,7 @@ const session = await stripe.checkout.sessions.create({
   customer_email: email,
   success_url: process.env.PUBLIC_URL + '/cart?success=1',
   cancel_url : process.env.PUBLIC_URL + '/cart?canceled=1',
-  metadata: {orderId: orderDoc._id.toString()},
+  metadata: {orderId: orderDoc._id.toString(),test:'ok'},
 
 });
 
--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,8 +1,9 @@
 import { mongooseConnect } from "@/lib/mongoose";
+import { Order } from "@/models/Order";
 const stripe = require('stripe')(process.env.STRIP_SK);
 import {buffer} from 'micro';
 
-const endpointSecret = "whsec_ea3eccd71ffae575dbe3baefbcb19993068fbee23fcbc4f632352136cb5b7e3b";
+const endpointSecret = "whsec_X78dwFSsi8UEDEDNUpQJYBjVmxhdwXu8";
 
 export default async function handler(req, res) {
     await mongooseConnect();
@@ -17,22 +18,29 @@ export default async function handler(req, res) {
       res.status(400).send(`Webhook Error: ${err.message}`);
       return;
     }
-  
+    
+
     // Handle the event
     switch (event.type) {
-      case 'payment_intent.succeeded':
-        const paymentIntentSucceeded = event.data.object;
-        console.log(paymentIntentSucceeded);
-        break;
+      case 'checkout.session.completed':
+        const data = event.data.object;
+        const orderId = data.metadata.orderId;
+        const paid = data.payment_status === 'paid';
+        if (orderId && paid) {
+          await Order.findByIdAndUpdate(orderId,{
+            paid:true,
+          })
+        }
       // ... handle other event types
       default:
         console.log(`Unhandled event type ${event.type}`);
     }
+    res.status(200).send('ok');
 }
 
 export const config = {
-    api: {bodyparser:false,}
+  api: {bodyParser:false,}
 };
 
-//revere-uplift-yay-blithe
+//prefer-grace-top-swift
 //acct_1OM5gKSAHn6S95rE
